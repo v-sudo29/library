aac4edc9334f2de5115a06e1ac9a1bbb3f8b5998
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-03-13 08:47:08 -0700

    Create function to loop over array, make cards

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myLibrary =[];
+const myLibrary =['ExampleBook1', 'ExampleBook2'];
 
 function Book() {
   // the constructor
@@ -7,13 +7,31 @@ function Book() {
 function addBookToLibrary() {
   // Obtain value of user input
   const bookTitle = document.querySelector('input').value;
+
+  // Add user input to myLibrary array
   myLibrary.push(bookTitle);
   return myLibrary;
 }
 
+function displayBooks() {
+  const cardDiv =  document.createElement('div');
+  cardDiv.setAttribute('class', 'card');
+  const bookTitleDiv = document.createElement('div');
+  const cardsContainer = document.querySelector('.cards-container')
+  
+  for (let i = 0; i < myLibrary.length; i++) {
+    const title = document.createTextNode(`${myLibrary[i]}`);
+    bookTitleDiv.appendChild(title);
+    cardDiv.appendChild(bookTitleDiv);
+
+    cardsContainer.appendChild(cardDiv);
+  }
+}
 
 // Create event listener for submit button
 const submitButton = document.querySelector('.submit-button');
 submitButton.addEventListener('click', () => {
   console.log(addBookToLibrary());
 });
+
+displayBooks();

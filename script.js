52e8917cf19b19c9fc896b1564799902d3d0b967
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-03-13 20:11:53 -0700

    Fix display books on page

--- script.js
+++ script.js
@@ -35,18 +35,17 @@ function addBookToLibrary() {
 
 // FUNCTION: Displays books onto page
 function displayBooks() {
-  const cardDiv =  document.createElement('div');
-  cardDiv.setAttribute('class', 'card');
-  const bookTitleDiv = document.createElement('div');
-  const bookAuthorDiv = document.createElement('div');
-  const bookPagesDiv = document.createElement('div');
-  const bookReadDiv = document.createElement('div');
-  const cardsOuterContainer = document.querySelector('.cards-outer-container');
-  const cardsInnerContainer = document.querySelector('.cards-inner-container');
-  const newCardsInnerContainer = document.createElement('div');
-  newCardsInnerContainer.setAttribute('class', 'cards-inner-container');
-  
-  for (let i = 0; i < myLibrary.length; i += 1) {
+  const numberOfBooks = myLibrary.length;
+
+  for (let i = numberOfBooks - 1; i < myLibrary.length; i += 1) {
+    const cardDiv =  document.createElement('div');
+    cardDiv.setAttribute('class', 'card');
+    const bookTitleDiv = document.createElement('div');
+    const bookAuthorDiv = document.createElement('div');
+    const bookPagesDiv = document.createElement('div');
+    const bookReadDiv = document.createElement('div');
+    const cardsInnerContainer = document.querySelector('.cards-inner-container');
+
     const title = document.createTextNode(`${myLibrary[i].title}`);
     const author = document.createTextNode(`${myLibrary[i].author}`);
     const pages = document.createTextNode(`${myLibrary[i].pages}`);
@@ -62,9 +61,8 @@ function displayBooks() {
     cardDiv.appendChild(bookPagesDiv);
     cardDiv.appendChild(bookReadDiv);
 
-    newCardsInnerContainer.appendChild(cardDiv);
+    cardsInnerContainer.appendChild(cardDiv);
   }
-  cardsOuterContainer.replaceChild(newCardsInnerContainer, cardsInnerContainer);
 }
 
 // BUTTON ('NEW BOOK'): open modal
